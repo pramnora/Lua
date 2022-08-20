3d75e3ede0ca651e49bb8ccaef56d3ce87c1ca95
Paul Ramnora <pramnora@yahoo.com>
2022-08-20 14:17:40 +0100

    Create mathOps.lua
    Added file: [mathOps.lua] Shows how to: add: +/subtract: -/multiply: */divide: (/)/use exponents: ^

---
+++ language/maths/operators/mathOps.lua
@@ -0,0 +1,14 @@
+--variable declaration...
+num = 3
+--main program/do output...
+print(num+num)
+print(num-num)
+print(num*num)
+print(num/num)
+print(num^num)
+--output...
+--6
+--0
+--9
+--1
+--27
