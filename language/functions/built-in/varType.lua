e1ebfe78915963b822c02ef6bd81f5ecbbe7c9e3
Paul Ramnora <pramnora@yahoo.com>
2022-08-20 13:27:41 +0100

    Create varType.lua
    Added file: [varType.lua] Shows how to view what 'type' a variable is...

---
+++ language/functions/built-in/varType.lua
@@ -0,0 +1,20 @@
+varName = 'a'
+io.write(varName,"\t",type(varName),"\n")
+
+varName = "Paul"
+io.write(varName,"\t",type(varName),"\n")
+
+varName = 123
+io.write(varName,"\t",type(varName),"\n")
+
+varName = 123.456
+io.write(varName,"\t",type(varName),"\n")
+
+--
+--output...
+--
+
+--a       string
+--Paul    string
+--123     number
+--123.456 number
