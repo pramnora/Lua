ad2de170b0986218904da8eb58f6e8b30fc05033
Paul Ramnora <pramnora@yahoo.com>
2022-08-20 11:27:43 +0100

    Create vars01a.lua
    Added file: [vars01a.lua]

---
+++ language/vars/vars01a.lua
@@ -0,0 +1,32 @@
+-- 
+-- variable declarations...
+--
+
+num1=1
+Num1=2
+NUM1=3
+
+--
+-- main program...
+--
+
+-- printout each variables value...
+
+print(num1)
+print(Num1)
+print(NUM1)
+
+-- print the sum total of all values...
+
+print("=")
+print(num1+Num1+NUM1)
+
+--
+-- output...
+--
+
+-- 1
+-- 2
+-- 3
+-- =
+-- 6
