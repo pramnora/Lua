dd20243a6a8fa3b82e8ce00fbe574ab7539500bd
Paul Ramnora <pramnora@yahoo.com>
2022-08-20 12:30:38 +0100

    Create varChange01.lua
    Added file: [varChange01.lua] Shows that it's possible to change variable type...by re-using the one same variable name...to become: string/integer/float/-etc.

---
+++ language/vars/simple/varChange01.lua
@@ -0,0 +1,14 @@
+varName = "Humpty "
+print(varName)
+
+varName= "Humpty Dumpty"
+print(varName)
+
+varName=1+2
+print(varName)
+
+varName=3.141592653589793
+print(varName)
+
+varName="Tel: 999"
+print(varName)
