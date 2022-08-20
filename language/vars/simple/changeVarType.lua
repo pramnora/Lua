6b5393cabd916a89785f56160f9915ee08c98f3c
Paul Ramnora <pramnora@yahoo.com>
2022-08-20 12:36:51 +0100

    Update changeVarType.lua
    Added explanatory code comments

--- language/vars/simple/changeVarType.lua
+++ language/vars/simple/changeVarType.lua
@@ -1,14 +1,19 @@
+-- word..
 varName = "Humpty "
 print(varName)
 
+-- multiple words...
 varName= "Humpty Dumpty"
 print(varName)
 
+-- integer/sum expression...
 varName=1+2
 print(varName)
 
+-- floating point...
 varName=3.141592653589793
 print(varName)
 
+-- words, and, numbers together...
 varName="Tel: 999"
 print(varName)
