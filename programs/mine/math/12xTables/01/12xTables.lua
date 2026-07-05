44b6eddc2ca1a47ef4fbeff6e3900d35656cc150
Paul Ramnora <pramnora@yahoo.com>
2026-07-05 19:32:51 +0100

    Add Lua script for 7 times table
    This script prints the multiplication table for 7 from 1 to 12.

---
+++ programs/mine/math/12xTables/01/12xTables.lua
@@ -0,0 +1,17 @@
+-- variable declarations
+
+tablesNo=7
+
+--do printout
+
+for timesNo = 1, 12 do
+    print("" .. timesNo .. " X " .. tablesNo .. " = " .. timesNo*tablesNo)
+end
+
+-- print out
+
+-- 1 x 7 = 7
+-- 2 x 7 = 14
+--  ...
+-- 11 x 7 = 77
+-- 12 x 7 = 84
