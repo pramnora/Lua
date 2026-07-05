5fec69c06a7b8314319b99fec9eeba99dea63e33
Paul Ramnora <pramnora@yahoo.com>
2026-07-05 21:46:49 +0100

    Add for loop to print fruits with their indices
    This script prints each fruit in the fruits table along with its index.

---
+++ language/loops/for2.lua
@@ -0,0 +1,15 @@
+-- variable declarations
+
+local fruits = {"apple","banana","pear"}
+
+-- print out each fruit
+
+for i,fruit in ipairs(fruits) do
+    print(i,fruit)
+end
+
+-- print out
+
+-- 1     apple
+-- 2     banana
+-- 3     pear
