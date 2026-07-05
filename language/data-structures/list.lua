6c18edd54a3dbdedb149c2ca65ee8da9e0e3a0f8
Paul Ramnora <pramnora@yahoo.com>
2026-07-05 19:56:49 +0100

    Create list.lua to display fruit names
    Add a Lua script to print a list of fruits.

---
+++ language/data-structures/list.lua
@@ -0,0 +1,15 @@
+-- list
+
+local fruits={"apple","banana","cherry"}
+
+-- do print out
+
+for i, fruit in ipairs(fruits) do
+    print(i, fruit)
+end
+
+-- print out
+
+-- 1 apple
+-- 2 banana
+-- 3 cherry
