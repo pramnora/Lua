464f284a24b1c4a5b09b6221f450124e53987260
Paul Ramnora <pramnora@yahoo.com>
2026-07-06 00:03:38 +0100

    Add print statements for records in dict2.lua
    Added print statements to display the number of records and each person's details.

--- language/data-structures/dict/dict2.lua
+++ language/data-structures/dict/dict2.lua
@@ -6,12 +6,19 @@ local people = {
     {name = "Jill",age=30}
 }
 
+-- print header
+
+print("No of records: " .. #people)
+
+-- print each record
+
 for i, person in ipairs(people) do
     print("Record no #" .. i .. ": " .. person.name .. ".. is  " .. person.age)
 end
 
 -- print out
 
+-- No of records: 3
 -- Record no #1: Harry is 25
 -- Record no #2: Jane is 8
 -- Record no #3: Jill is 30
