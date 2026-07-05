4e28cb51005f2695496db2066aca8950549d5969
Paul Ramnora <pramnora@yahoo.com>
2026-07-05 18:38:12 +0100

    Add basic arithmetic operations in maths-ops2.lua

---
+++ language/maths/operators/maths-ops2.lua
@@ -0,0 +1,6 @@
+num1=3;num2=3
+print("" .. num1 .. " + " .. num2.. " = " .. num1+num2) -- 6
+print("" .. num1 .. " - " .. num2.. " = " .. num1-num2) -- 0
+print("" .. num1 .. " * " .. num2.. " = " .. num1*num2) -- 9
+print("" .. num1 .. " / " .. num2.. " = " .. num1/num2) -- 1.0
+print("" .. num1 .. " ^ " .. num2.. " = " .. num1^num2) -- 27.0
