2042160912851115bb70cd2172229ea6704ac731
Paul Ramnora <pramnora@yahoo.com>
2026-07-05 21:53:21 +0100

    Add countdown while loop in while1.lua
    Implement a while loop to count down from 5.

---
+++ language/loops/while1.lua
@@ -0,0 +1,18 @@
+-- variable declarations
+
+local count=5
+
+-- man program
+
+while count > 0 do
+     print("Count: " .. count)
+     count = count - 1
+end
+
+-- print out
+
+-- Count: 5
+-- Count: 4
+-- Count: 3
+-- Count: 2
+-- Count: 1
