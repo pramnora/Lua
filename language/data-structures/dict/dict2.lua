a2ec83f0d9f72d96e4e5a011a927bc6c0200c834
Paul Ramnora <pramnora@yahoo.com>
2026-07-05 23:59:53 +0100

    Modify print output to show record numbers
    Updated print statement to include record number.

--- language/data-structures/dict/dict2.lua
+++ language/data-structures/dict/dict2.lua
@@ -7,12 +7,12 @@ local people = {
 }
 
 for i, person in ipairs(people) do
-    print(person.name .. " is " .. person.age)
+    print("Record no #" .. i .. ": " .. person.name .. ".. is  " .. person.age)
 end
 
 -- print out
 
--- Harry is 25
--- Jane is 8
--- Jill is 30
+-- Record no #1: Harry is 25
+-- Record no #2: Jane is 8
+-- Record no #3: Jill is 30
 
