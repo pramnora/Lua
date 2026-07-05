3b021e6c418512c7ab05c33f52f2e045877e686c
Paul Ramnora <pramnora@yahoo.com>
2026-07-05 18:39:54 +0100

    Add print statements for basic math operations

--- language/maths/operators/maths-ops2.lua
+++ language/maths/operators/maths-ops2.lua
@@ -1,4 +1,6 @@
+-- variable declarations
 num1=3;num2=3
+-- do printout
 print("" .. num1 .. " + " .. num2.. " = " .. num1+num2) -- 6
 print("" .. num1 .. " - " .. num2.. " = " .. num1-num2) -- 0
 print("" .. num1 .. " * " .. num2.. " = " .. num1*num2) -- 9
